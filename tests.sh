3ccac094f48cf38b15f135d1e37ba07ffb88d60e
Ariel Chenet <apchenet@gmail.com>
2021-09-04 17:32:14 +0200

    Add tests.

--- tests.sh
+++ tests.sh
@@ -8,5 +8,6 @@ go build -o find-duplicates-or-reverse solution/*.go
 # Test with various test files
 ./find-duplicates-or-reverse test-files/1
 ./find-duplicates-or-reverse test-files/2
+./find-duplicates-or-reverse test-files/3
+./find-duplicates-or-reverse test-files/4
 
-# Test with command line input
